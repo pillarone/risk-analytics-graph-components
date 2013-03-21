0adcaa94c866f90fef14222b885bf25c34255111
detlef-brendle <detlef.brendle@canoo.com>
2013-03-21 10:40:12 +0100

    PMO-2436 upgrade component to grails 2.2.1

--- RiskAnalyticsGraphComponentsGrailsPlugin.groovy
+++ RiskAnalyticsGraphComponentsGrailsPlugin.groovy
@@ -2,9 +2,9 @@ import org.pillarone.riskanalytics.core.util.ResourceBundleRegistry
 
 class RiskAnalyticsGraphComponentsGrailsPlugin {
     // the plugin version
-    def version = "0.1.5"
+    def version = "0.1.6"
     // the version or versions of Grails the plugin is designed for
-    def grailsVersion = "1.3.7 > *"
+    def grailsVersion = "2.2.1 > *"
     // the other plugins this plugin depends on
     def dependsOn = [:]
     // resources that are excluded from plugin packaging
