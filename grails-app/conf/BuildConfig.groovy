51ad4be597498fc391f192a3dc20c0632f0b7988
detlef-brendle <detlef.brendle@canoo.com>
2013-04-03 15:14:58 +0200

    new core plugin version

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -25,7 +25,7 @@ grails.project.dependency.resolution = {
         compile ":excel-import:0.9.6"
 
         if (appName == "risk-analytics-graph-components") {
-            runtime "org.pillarone:risk-analytics-core:1.7-a2"
+            runtime "org.pillarone:risk-analytics-core:1.7-a6"
             runtime("org.pillarone:risk-analytics-commons:0.5") { transitive = false }
         }
     }
