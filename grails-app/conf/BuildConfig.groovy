9d4b5240c81b66675f6df6738e4909f5cf94c1b3
Stefan Kunz <stefan.kunz@intuitive-collaboration.com>
2014-04-16 16:29:29 +0200

    fix build by updating quartz plugin version

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -17,7 +17,7 @@ grails.project.dependency.resolution = {
         runtime ":hibernate:2.2.1"
         runtime ":joda-time:0.5"
         runtime ":maven-publisher:0.7.5"
-        runtime ":quartz:0.4.2"
+        runtime ":quartz:1.0.1"
         runtime ":spring-security-core:1.1.2"
         runtime ":tomcat:2.2.1"
 
